531288c879e4eee6a5882e37305355cf9fad5dfd
zuzanapiorkowska <92804777+zuzanapiorkowska@users.noreply.github.com>
2021-12-15 21:49:40 +0100

    Add files via upload

---
+++ 17. Movie App/script.js
@@ -0,0 +1,62 @@
+const apiUrl = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4ed6cfbde7e10eeb22d3093243e7ea2b";
+const imgPath = "https://image.tmdb.org/t/p/w1280"
+const searchApi = 'https://api.themoviedb.org/3/search/movie?api_key=4ed6cfbde7e10eeb22d3093243e7ea2b&query="';
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const main = document.getElementById("main");
+
+async function getMovies(url){
+    const res = await fetch(url);
+    const data = await res.json();
+
+    showMovies(data.results);
+
+}
+
+function showMovies(movies) {
+    main.textContent = "";
+    movies.forEach((movie) => {
+        const {title, poster_path, vote_average, overview } = movie;
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+        movieEl.innerHTML = `
+        <img src="${imgPath + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                <p>${overview}</p>
+            </div>`
+
+    main.appendChild(movieEl);
+    })
+}
+
+function getClassByRate(vote) {
+ if(vote>=8) {
+     return "green";
+ } else if (vote<5) {
+     return "red";
+ } else {
+     return "orange";
+ }
+}
+
+form.addEventListener ("submit", (e)=>{
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm && searchTerm !== "") {
+        getMovies(searchApi + searchTerm + '"');
+        search.value="";
+    } else {
+        window.location.reload();
+    }
+})
+
+
+getMovies(apiUrl);
+
